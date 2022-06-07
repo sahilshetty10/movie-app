a8aef326da2b2fc0ebf1299ba925f8a616bd471a
sahilshetty10 <sahilshetty69@gmail.com>
2022-06-07 23:04:12 +0530

    removed extra code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
+  BrowserRouter as Router
 } from "react-router-dom";
 
 
